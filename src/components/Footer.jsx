87caeae897de2b0a9fa71846ef7ae070ea666586
Itamar Joire <ijoire7@gmail.com>
2024-02-13 18:56:25 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export function Footer(){
         <div className='md:flex md:flex-row-reverse md:justify-end md:items-center md:gap-12'> 
           <div className="flex gap-12">
             <div>
-              <ul className="grid gap-3">
+              <ul className="grid gap-4">
                 <li className="text-white text-sm font-normal"><a href="/">Sobre</a></li>
                 <li className="text-white text-sm font-normal"><a href="/">Serviços</a></li>
                 <li className="text-white text-sm font-normal"><a href="/">Depoimentos</a></li>
@@ -17,12 +17,13 @@ export function Footer(){
               </ul>
             </div>
           </div>
-
-          <div className='mt-10 pb-12'>
-              <h2 className='text-white font-bold text-3xl'>Dinar Decor</h2>
-              <ul className='flex gap-3 mt-4'>
-                <li><a href="/"><FaWhatsapp  className='text-white' size={24}/></a></li>
-                <li><a href="/"><FaInstagram className='text-white' size={24}/></a></li>
+        
+          <div className='mt-8 border-t-[1px] border-[#CDCDCD] border-opacity-30 rounded'></div>
+          <div className=' text-[#CDCDCD] text-sm font-medium text-center py-8'>
+            © Desde 2024 - Todos os direitos reservados
+            <ul className='flex gap-3 mt-4 justify-center'>
+                <li><a href="/"><FaWhatsapp  className='text-[#CDCDCD] ' size={24}/></a></li>
+                <li><a href="/"><FaInstagram className='text-[#CDCDCD]' size={24}/></a></li>
               </ul>
           </div>
         </div>
