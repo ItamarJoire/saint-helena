3748ada4b42fa95fc3da93de8eaa1a67c4087c6c
itamarjoire <ijoire7@gmail.com>
2024-02-21 13:48:14 -0300

    hero

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,7 +3,7 @@ import { Carousel } from "./carousel";
 
 export function Services(){
   return(
-    <div className="bg-hero-pattern-nature bg-cover bg-no-repeat pb-24">
+    <div className="bg-hero-pattern-nature bg-cover bg-no-repeat pb-56">
       <section id="services" className="h-auto">
         <Grid>
           <h1 className="font-['Bungee'] text-primary text-xl text-center font-extrabold tracking-widest">Na Saint Helena seu <span className="text-secondary-yellow">filho tem contato</span> com a natureza
