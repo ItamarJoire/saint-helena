bb4a8ab1bc1d3bf403a627826b1ed77abf1406a0
Itamar Joire <ijoire7@gmail.com>
2024-02-25 13:44:12 -0300

    Structure

--- src/pages/structureSalvador.jsx
+++ src/pages/structureSalvador.jsx
@@ -1,7 +1,12 @@
+import { Grid } from "../containers";
+
 export function StructureSalvador(){
   return(
-    <section className="mt-24">
-      <h1 className="font-bold text-emerald-500">Estrutura Salvador</h1>
+    <section className="mt-20">
+      <Grid>
+        <h1 className="font-['Bungee'] text-primary text-2xl text-center font-extrabold tracking-widest">Estrutura</h1>
+        <p className="mt-4 text-center text-xl text-gray-400 ">Saiba mais sobre o que Saint Helena Salvador pode oferecer</p>
+      </Grid>
     </section>
   )
 }
