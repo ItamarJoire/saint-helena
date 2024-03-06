9d371bc70f1606c1ef8635c213d52378bfcf8e3e
Itamar Joire <ijoire7@gmail.com>
2024-03-06 20:06:46 -0300

    Page Structure

--- src/pages/structureSalvador.jsx
+++ src/pages/structureSalvador.jsx
@@ -10,18 +10,18 @@ export function StructureSalvador(){
     <section>
       <Grid>
         <div className="">
-          <img className="absolute size-44 opacity-20 top-[30px] left-[-120px]" src={Circle3} alt="" />
-          <img className="absolute size-44 opacity-20 bottom-[0px] right-[-140px]" src={Circle3} alt="" />
+          <img className="absolute size-44 opacity-15 top-[80px] left-[-120px] lg:top-[160px] lg:left-[-100px]" src={Circle3} alt="" />
+          <img className="absolute size-44 opacity-20 bottom-[0px] right-[-140px] lg:right-[-90px]" src={Circle3} alt="" />
 
           <h1 className="font-['Bungee'] text-primary text-2xl text-center font-extrabold tracking-widest">Estrutura</h1>
-          <p className="mt-4 mx-16 mb-16 text-center text-xl text-gray-500 ">Saiba mais sobre o que Saint Helena Salvador pode oferecer</p>
+          <p className="mt-4 mx-16 mb-16 text-center text-xl text-gray-500 lg:mx-44">Saiba mais sobre o que Saint Helena Salvador pode oferecer</p>
 
-          <div className="relative bg-primary w-[400px] mx-auto rounded-xl lg:w-full ">
-            <img className="absolute size-20 right-0 top-[-40px]" src={Mascot} alt="" />
-            <div className="lg:flex ">
-              <img src={Local} alt="" className="p-1 rounded-t-xl h-[300px] w-[400px] lg:rounded-tr-none lg:rounded-bl-xl lg:w-auto"/>
-              <div className="py-20 px-6 lg:py-6 lg:max-w-[455px]">
-                <h1 className="font-['Bungee'] text-primary-2 text-2xl text-center font-extrabold tracking-widest lg:text-left">Espaço com mais de 1.500m²</h1>
+          <div className="relative bg-primary max-w-[400px] mx-auto rounded-xl sm:max-w-none sm:mx-0 lg:mx-24">
+            <img className="absolute size-20 right-0 top-[-40px] sm:top-[-60px]" src={Mascot} alt="" />
+            <div className="sm:flex">
+              <img src={Local} alt="" className="p-1 rounded-t-xl h-[300px] w-full sm:rounded-tr-none sm:rounded-bl-xl sm:w-[300px] sm:h-auto "/>
+              <div className="py-12 px-6 sm:py-8 mx-auto">
+                <h1 className="font-['Bungee'] text-primary-2 text-2xl text-center font-extrabold tracking-widest sm:text-left">Espaço com mais de 1.500m²</h1>
                 <p className="mt-4 text-left text-xl text-white ">
                   A Saint Helena está preparada para oferecer a melhor estrutura para os seus alunos, com amplo espaço e contato com a natureza.
                 </p>
