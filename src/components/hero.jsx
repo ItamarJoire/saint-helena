6be18544fe96d20d0d8d26205c7df0853c377bd4
Itamar Joire <ijoire7@gmail.com>
2024-04-30 18:27:48 -0300

    12 para 15

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -19,7 +19,7 @@ export function Hero() {
           <div className='py-16 bg-gradient-to-t from-primary-dark to-primary sm:flex sm:justify-center sm:items-center'>
             <div className='mx-3 py-12 sm:mx-12'>
               <div className='flex flex-col items-center sm:items-start sm:max-w-[500px]'>
-                <h1 className="font-['Bungee'] text-primary-2 text-2xl text-center sm:text-left font-extrabold tracking-widest xl:text-3xl">12 anos de excelência em <span className="text-secondary-yellow">educação bilíngue</span></h1>
+                <h1 className="font-['Bungee'] text-primary-2 text-2xl text-center sm:text-left font-extrabold tracking-widest xl:text-3xl">15 anos de excelência em <span className="text-secondary-yellow">educação bilíngue</span></h1>
                 <h3 className="mt-3 text-primary-2 text-center sm:text-left opacity-90 text-xl font-medium xl:text-2xl">Inspirada no sonho de um avô visionário em Maringá.</h3> 
                 <Link to='/educacao-infantil'> 
                   <button className="font-['Bungee'] mt-8 text-xl shadow-md font-medium rounded-lg px-6 py-3 bg-gradient-to-t from-secondary-yellow-dark to-secondary-yellow text-white hover:opacity-90 duration-200">
